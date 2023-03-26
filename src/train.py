deda30a55227f52f50254cc6d0f1e56df35eaee1
usman-icustoms <usman.ali@icustoms.ai>
2023-03-26 16:16:52 +0500

    updated training code

--- src/train.py
+++ src/train.py
@@ -1,29 +1,21 @@
-# pip install pycaret
-# pip install --user mlflow
-
 # Importing necessary libraries
 import pandas as pd
 from pycaret.classification import *
-
-import pandas as pd
   
 # Reading the data
-data = pd.read_csv('/content/drug_classification.csv')
-
-
+data = pd.read_csv('data\drug_classification.csv')
 
 # Initializing classification setup
 clf = setup(data=data, target='Drug', train_size=0.86, session_id=123, use_gpu = True, experiment_name='classifying_drugs')
 
-print(clf)
-
 # Comparing models
 best_model = compare_models(sort='F1', n_select=1)
 
+# predict on test set
+holdout_pred = predict_model(best_model)
+
 # Evaluating the best model
-evaluate_model(best_model)
-
-
+# evaluate_model(best_model)
 
 # Saving the best model
 save_model(best_model, 'classification')
